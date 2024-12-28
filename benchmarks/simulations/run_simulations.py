03697d5e6165993aafae85bead14de5f63e0d7f9
ehuangc <edwardhuang02@gmail.com>
2024-12-28 15:29:13 -0500

    Calculate mean metrics from simulations

--- benchmarks/simulations/run_simulations.py
+++ benchmarks/simulations/run_simulations.py
@@ -1,19 +1,27 @@
+from collections import defaultdict
 from simulated_pedigree import SimulatedPedigree
 
 
-def simulate():
+def simulate() -> dict[str, float]:
     simulated_pedigree = SimulatedPedigree()
     simulated_pedigree.create_pedigree()
     simulated_pedigree.mask_data()
     simulated_pedigree.run_algorithm()
     metrics = simulated_pedigree.get_metrics()
-    print(metrics)
+    return metrics
 
-def run_experiment():
-    simulate()
+def run_experiment(num_simulations: int = 100) -> dict[str, float]:
+    experiment_metrics = defaultdict(list)
+    for _ in range(num_simulations):
+        metrics = simulate()
+        for metric, value in metrics.items():
+            experiment_metrics[metric].append(value)
+    
+    mean_metrics = {metric: sum(values) / len(values) for metric, values in experiment_metrics.items()}
+    print(mean_metrics)
 
 def main():
-    run_experiment()
+    run_experiment(num_simulations=10)
 
 if __name__ == "__main__":
     main()
