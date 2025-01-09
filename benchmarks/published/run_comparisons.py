0ce1f98676dc4c4708329f464c1fde5b3b72875b
ehuangc <edwardhuang02@gmail.com>
2025-01-08 22:43:42 -0800

    Add Rivollat pedigree information

--- benchmarks/published/run_comparisons.py
+++ benchmarks/published/run_comparisons.py
@@ -2,10 +2,10 @@ import os
 from relation_comparison import RelationComparison
 
 def main():
-    data_dir = os.path.join(os.path.dirname(__file__), "fowler")
-    published_relations_path = os.path.join(data_dir, "published_relations.csv")
-    algorithm_nodes_path = os.path.join(data_dir, "algorithm_nodes.csv")
-    algorithm_relations_path = os.path.join(data_dir, "algorithm_relations_custom.csv")
+    data_dir = os.path.join(os.path.dirname(__file__), "rivollat")
+    published_relations_path = os.path.join(data_dir, "relations_published.csv")
+    algorithm_nodes_path = os.path.join(data_dir, "nodes.csv")
+    algorithm_relations_path = os.path.join(data_dir, "relations_READv2.csv")
 
     relation_comparison = RelationComparison(published_relations_path=published_relations_path,
                                              algorithm_nodes_path=algorithm_nodes_path,
