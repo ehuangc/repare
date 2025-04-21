dcdc5ce9b92421cf664486f611ceecab0d9bc10d
ehuangc <edwardhuang02@gmail.com>
2025-04-21 03:28:59 -0700

    Write published data results to file

--- benchmarks/published/run_comparisons.py
+++ benchmarks/published/run_comparisons.py
@@ -6,20 +6,41 @@ from tqdm.contrib.logging import logging_redirect_tqdm
 
 
 def main():
-    data_dir = os.path.join(os.path.dirname(__file__), "data", "fowler")
-    published_relations_path = os.path.join(data_dir, "published_exact_relations.csv")
-    algorithm_nodes_path = os.path.join(data_dir, "nodes.csv")
-    algorithm_relations_path = os.path.join(data_dir, "inferred_relations_coeffs.csv")
+    results_dir = os.path.join(os.path.dirname(__file__), "results")
+    os.makedirs(results_dir, exist_ok=True)
+    results_path = os.path.join(results_dir, "results.csv")
 
-    logging.basicConfig(level=logging.WARNING)  # Set to logging.INFO for more detailed output
-    with logging_redirect_tqdm():
-        relation_comparison = RelationComparison(
-            published_relations_path=published_relations_path,
-            algorithm_nodes_path=algorithm_nodes_path,
-            algorithm_relations_path=algorithm_relations_path,
-        )
-        for relation, count in relation_comparison.get_metrics().items():
-            print(f"{relation}: {round(count, 2)}")
+    for idx, (site, relations_file_name) in enumerate(
+        [
+            ("hazelton_north", "inferred_relations_coeffs.csv"),
+            ("hazelton_north", "inferred_relations_custom.csv"),
+            ("nepluyevsky", "inferred_relations_KIN.csv"),
+            ("nepluyevsky", "inferred_relations_custom.csv"),
+            ("gurgy", "inferred_relations_READv2.csv"),
+        ]
+    ):
+        print(f"Reconstructing pedigree: site={site}, relation_data={relations_file_name}")
+        data_dir = os.path.join(os.path.dirname(__file__), "data", site)
+        algorithm_nodes_path = os.path.join(data_dir, "nodes.csv")
+        algorithm_relations_path = os.path.join(data_dir, relations_file_name)
+        published_relations_path = os.path.join(data_dir, "published_exact_relations.csv")
+
+        logging.basicConfig(level=logging.WARNING)  # Set to logging.INFO for more detailed output
+        with logging_redirect_tqdm():
+            relation_comparison = RelationComparison(
+                published_relations_path=published_relations_path,
+                algorithm_nodes_path=algorithm_nodes_path,
+                algorithm_relations_path=algorithm_relations_path,
+            )
+
+            with open(results_path, "a") as file:
+                metrics_values = relation_comparison.get_metrics()
+                metrics = list(metrics_values.keys())
+                values = list(metrics_values.values())
+                if idx == 0:
+                    file.truncate(0)
+                    file.write(",".join(["site", "relation_data"] + list(metrics)) + "\n")
+                file.write(f"{site},{relations_file_name}," + ",".join([str(value) for value in values]) + "\n")
 
 
 if __name__ == "__main__":
