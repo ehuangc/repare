589ab5bb0b817816dc1f0481c42807fd166c3d21
ehuangc <edwardhuang02@gmail.com>
2025-06-25 00:32:38 -0400

    Move inferred/published analysis code to utils file

--- benchmarks/published/analyze_gurgy_pedigrees.py
+++ benchmarks/published/analyze_gurgy_pedigrees.py
@@ -1,46 +1,13 @@
 import os
-import tempfile
 
-import matplotlib.pyplot as plt
+from comparator.analysis_utils import (
+    get_mt_colormap,
+    get_published_pedigree,
+    plot_inferred_pedigree,
+    plot_published_pedigree,
+    write_relation_differences,
+)
 from comparator.relation_comparison import RelationComparison
-
-from repare.pedigree import Pedigree
-from repare.pedigree_reconstructor import PedigreeReconstructor
-
-
-def get_published_pedigree(nodes_path: str, relations_path: str) -> Pedigree:
-    # Write outputs other than the plot to a temporary directory
-    with tempfile.TemporaryDirectory() as temp_dir:
-        pedigree_reconstructor = PedigreeReconstructor(
-            relations_path, nodes_path, outputs_dir=temp_dir, max_candidate_pedigrees=1000, plot=False
-        )
-        published_pedigree = pedigree_reconstructor.find_best_pedigree()
-    return published_pedigree
-
-
-def plot_inferred_pedigree(inferred_pedigree: Pedigree, plot_path: str, mt_haplogroup_to_color: dict[str, str]) -> None:
-    inferred_pedigree.plot(path=plot_path, mt_haplogroup_to_color=mt_haplogroup_to_color)
-
-
-def plot_published_pedigree(
-    published_pedigree: Pedigree,
-    plot_path: str,
-    mt_haplogroup_to_color: dict[str, str] | None = None,
-    nodes_to_remove: list[str] | None = None,
-    edges_to_remove: list[tuple[str, str]] | None = None,
-    dotted_edges_to_add: list[tuple[str, str]] | None = None,
-) -> None:
-    published_pedigree.plot(
-        path=plot_path,
-        mt_haplogroup_to_color=mt_haplogroup_to_color,
-        nodes_to_remove=nodes_to_remove,
-        edges_to_remove=edges_to_remove,
-        dotted_edges_to_add=dotted_edges_to_add,
-    )
-
-
-def write_relation_differences(relation_comparison: RelationComparison, path: str):
-    relation_comparison.write_relation_differences(path=path)
 
 
 def main():
@@ -61,25 +28,7 @@ def main():
         relations_path=os.path.join(data_dir, "published_exact_relations.csv"),
     )
 
-    # Build mt_haplogroup color mapping so both plots use the same colormap
-    inferred_pedigree_mt_haplogroups = set(
-        [
-            inferred_pedigree.node_to_data[node]["mt_haplogroup"].replace("*", "")
-            for node in inferred_pedigree.node_to_data
-            if not node.isnumeric()
-        ]
-    )
-    published_pedigree_mt_haplogroups = set(
-        [
-            published_pedigree.node_to_data[node]["mt_haplogroup"].replace("*", "")
-            for node in published_pedigree.node_to_data
-            if not node.isnumeric()
-        ]
-    )
-    mt_haplogroups = sorted(inferred_pedigree_mt_haplogroups | published_pedigree_mt_haplogroups)
-    cmap = plt.get_cmap("tab20")
-    mt_haplogroup_to_color = {haplogroup: cmap(i / len(mt_haplogroups)) for i, haplogroup in enumerate(mt_haplogroups)}
-
+    mt_haplogroup_to_color = get_mt_colormap(inferred_pedigree, published_pedigree)
     plot_inferred_pedigree(
         inferred_pedigree,
         plot_path=os.path.join(results_dir, "inferred_pedigree.png"),
