379ccadd12725ead22247991483ad0b0d3855fc7
ehuangc <edwardhuang02@gmail.com>
2025-06-27 00:05:15 -0400

    Specify F1 "scores" in heatmaps

--- benchmarks/simulations/plot_parameter_data.py
+++ benchmarks/simulations/plot_parameter_data.py
@@ -87,7 +87,7 @@ def plot_results(results_dir: str) -> None:
             heatmap_data, annot=True, fmt=".2f", cmap="Blues", cbar_kws={"label": metric}, vmin=0.5, vmax=1.0
         )
         ax.figure.axes[-1].yaxis.label.set_size(12)  # Set colorbar label size
-        plt.title(f"{metric} Heatmap", fontsize=14)
+        plt.title(f"{metric} Scores", fontsize=14)
         plt.xlabel("Kinship Relation Error Rate Scale", fontsize=12)
         plt.ylabel("p(Mask Node)", fontsize=12)
         plt.savefig(f"results/parameter_experiment/plots/{metric.lower().replace(' ', '_')}_heatmap.png", dpi=600)
